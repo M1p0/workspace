d08c627ca82501b84227718e1589353a5fa31a25
M1p0 <lx669196851@gmail.com>
2017-04-04 19:49:49 +0800

    Update FileIO.h

--- Lib/Lib/FileIO.h
+++ Lib/Lib/FileIO.h
@@ -5,9 +5,10 @@ using namespace std;
 class FileIO
 {
 public:
-    void Create(string location);//ÈçFile.Create("c:\\test.txt")
+    void Create(string location);//ÃˆÃ§File.Create("c:\\test.txt")
     void Write(string location, string data);
     void Read(string location);
     void CopyFile(char *SourceFile, char *NewFile);
     void DeleteFile(string location);
+    //
 };
