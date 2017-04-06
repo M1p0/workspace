4d3910ddcc72a4ca7c455e06b61efeffe8e87f2f
M1p0 <lx669196851@gmail.com>
2017-04-06 09:01:01 +0800

    Update command.cpp

--- Lib/Test/command.cpp
+++ Lib/Test/command.cpp
@@ -1,4 +1,24 @@
-void command()
-{
+#include <iostream>
+#include "FileIO.h"
+#include <string>
+using namespace std;
 
+string command(FileIO &File)
+{
+    string cmd;
+    cin>>cmd;
+    if(cmd=="CopyFile")
+    {
+        File.CopyFile()
+        //cout<<"CopyFile";
+    }
+    return cmd;
+}
+
+int main()
+{
+    string str;
+    str=command();    
+
+    system("pause");
 }
