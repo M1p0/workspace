ca73fe6a4e660aa848730c6975ae2b9480a54b67
M1p0 <lx669196851@gmail.com>
2017-09-25 00:12:21 +0800

    timecost
    This is a beta function.It can calculate how much time your functions cost.

---
+++ Lib/Lib/timecost.cpp
@@ -0,0 +1,27 @@
+//beta function
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+int add(int a, int b)
+{
+    cout << a + b << endl;
+    return a + b;
+}
+
+void timecost(int(*ptr)(int a, int b), int a, int b)
+{
+    DWORD start;
+    DWORD end;
+    start = GetTickCount();
+    ptr(a, b);
+    end = GetTickCount();
+    cout << "time cost:" << end - start << endl;
+}
+
+//int main()
+//{
+//    timecost(add, 2, 3);
+//    return 0;
+//}
